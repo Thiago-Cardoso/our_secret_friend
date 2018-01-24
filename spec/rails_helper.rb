3136c743f3aa4a2a40cf58ad0f81daf93fefed51
Thiago-Cardoso <trovador182@gmail.com>
2018-01-24 20:42:27 -0200

    create service Raffle + Set devise in rspec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,6 +8,10 @@ require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
+#configure devise
+RSpec.configure do |config|
+  config.include Devise::Test::ControllerHelpers, type: :controller
+end
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
