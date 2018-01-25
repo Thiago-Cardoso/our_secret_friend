10ac84773391b40209e2f0a0047c14acd00466e4
Thiago-Cardoso <trovador182@gmail.com>
2018-01-25 21:09:52 -0200

    set model campaign and set model member

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,3 +1,22 @@
 class Campaign < ApplicationRecord
   belongs_to :user
+  has_many :members, dependent: :destroy #one campaign has many member, dependent according remove
+
+ #before create call method set_status in pending
+  before_create :set_status
+  #before create call method
+  before_create :set_member
+  enum status: [:pending, :finished]
+  validates :title, :description, :user, :status, presence: true
+
+  def set_status
+    self.status = :pending # when not do raffle
+  end
+
+  #when create campaign always is a member
+  def set_member
+    #create a member and associated a campaign
+    self.members << Member.create(name: self.user.name, email: self.user.email)
+  end
+
 end
--- app/models/member.rb
+++ app/models/member.rb
@@ -1,3 +1,26 @@
 class Member < ApplicationRecord
   belongs_to :campaign
+  after_save :set_campaign_pending
+  #it is required, name, email, campaign
+  validates :name, :email, :campaign, presence: true
+
+#look the email is open
+#token url variable is randon
+  def set_pixel
+   self.open = false
+   self.token = loop do
+     #if exist a member with the same token raffle the other else if member exist break
+     random_token = SecureRandom.urlsafe_base64(nil, false)
+     break random_token unless Member.exists?(token: random_token)
+   end
+   self.save!
+ end
+
+  protected
+
+  def set_campaign_pending
+    #after the save update campaign for status pending
+    self.campaign.update(status: :pending)
+  end
+
 end
