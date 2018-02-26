4d141e63fea4ca2e8b000586973777fdfeac7bd5
Thiago-Cardoso <trovador182@gmail.com>
2018-02-26 18:46:25 -0300

    test of job Raffle

--- spec/jobs/campaign_raffle_job_spec.rb
+++ spec/jobs/campaign_raffle_job_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe CampaignRaffleJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+  #pending "add some examples to (or delete) #{__FILE__}"
+    describe "#perform_later" do
+    it "test of job" do
+      ActiveJob::Base.queue_adapter = :test
+      expect {
+        CampaignRaffleJob.perform_later('emails')
+      }.to have_enqueued_job
+    end
+  end
 end
