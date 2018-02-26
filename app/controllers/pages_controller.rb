4673ef1963fafe83f2592020064b31348a90675e
Thiago-Cardoso <trovador182@gmail.com>
2018-02-26 20:11:28 -0300

    modification of view home

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,13 @@
 class PagesController < ApplicationController
   def home
+    #verification if user is logged
+    if current_user
+      @campaigns = current_user.campaigns
+      # if exist any register campaign for user, redirect for campaign
+        if @campaigns.any? == true
+          redirect_to "/campaigns/"
+        end
+    end
   end
+
 end
