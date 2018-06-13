ce9391f14edd62550d1f8eadea748f98ee3f3756
Thiago-Cardoso <trovador182@gmail.com>
2018-06-12 21:31:11 -0300

    adjustment in campaign mailer

--- app/mailers/campaign_mailer.rb
+++ app/mailers/campaign_mailer.rb
@@ -7,7 +7,7 @@ class CampaignMailer < ApplicationMailer
   #
   def raffle(campaign, member, friend)
     @campaign = campaign
-    @member = membraffle_errorer
+    @member = member
     @friend = friend
     mail to: @member.email, subject: "Nosso Amigo Secreto: #{@campaign.title}"
   end
