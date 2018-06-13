20958993a57a188e96967e623fc7e6ff8fd3a266
Thiago-Cardoso <trovador182@gmail.com>
2018-06-12 21:20:59 -0300

    test of job

--- spec/jobs/campaign_raffle_job_spec.rb
+++ spec/jobs/campaign_raffle_job_spec.rb
@@ -1,12 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe CampaignRaffleJob, type: :job do
-  #pending "add some examples to (or delete) #{__FILE__}"
-    describe "#perform_later" do
-    it "test of job" do
+  include ActiveJob::TestHelper
+
+  let(:campaign) { create(:campaign) }
+  let(:member) { create(:member, campaign: @campaign) }
+  let(:friend) { create(:member, campaign: @campaign) }
+  let(:mail) { CampaignMailer.raffle(@campaign, @member, @friend) }
+
+  describe "#email job" do
+
+    it "when send email for more one members" do
       ActiveJob::Base.queue_adapter = :test
       expect {
-        CampaignRaffleJob.perform_later('emails')
+        CampaignRaffleJob.perform_later('raffle')
       }.to have_enqueued_job
     end
   end
